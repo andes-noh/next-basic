07c80528127c329cd828e46b18347b8587b1aa7f
andes-noh <nkj9693@gmail.com>
2022-09-01 17:48:20 +0900

    chore(signup): 회원가입 화면 추가

---
+++ pages/signup/signup.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+import styled from '@emotion/styled'
+import { SignUp } from '../../page/Signup/signup'
+
+export const Page: React.FC = () => {
+  return <SignUp />
+}
+
+export default Page
